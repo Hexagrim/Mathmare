f5a93e3c821e5d25086f74043bff461cbd88665e
HexaGrim <hexagrim3456@gmail.com>
2026-08-30 13:02:10 +0545

    hr5, tryna make the object hold and drop thing, so many things to make still

--- Assets/Scripts/PickupScript.cs
+++ Assets/Scripts/PickupScript.cs
@@ -1,16 +1,123 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 public class PickupScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public GameObject player;
+    public Transform holdPos;
+
+    public float throwForce = 500f;
+    public float pickUpRange = 100f;
+    private GameObject heldObj;
+    private Rigidbody heldObjRb;
+    private int LayerNumber;
+
     void Start()
     {
-        
+        LayerNumber = LayerMask.NameToLayer("holdLayer");
     }
 
-    // Update is called once per frame
     void Update()
     {
+       
+        Debug.DrawRay(transform.position, transform.forward * pickUpRange, Color.red);
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (heldObj == null)
+            {
+                RaycastHit hit;
+
+                if (Physics.Raycast(transform.position, transform.forward, out hit, pickUpRange))
+                {
+                    if (hit.transform.gameObject.tag == "canPickUp")
+                    {
+                        PickUpObject(hit.transform.gameObject);
+                    }
+                }
+            }
+            else
+            {
+                StopClipping();
+                DropObject();
+                StopClipping();
+            }
+        }
+
+        if (heldObj != null)
+        {
+            MoveObject();
+
+        }
+    }
+
+    void PickUpObject(GameObject pickUpObj)
+    {
+        if (pickUpObj.GetComponent<Rigidbody>())
+        {
+            heldObj = pickUpObj;
+            heldObjRb = pickUpObj.GetComponent<Rigidbody>();
+            heldObjRb.isKinematic = true;
+            heldObjRb.transform.parent = holdPos.transform;
+            heldObj.layer = LayerNumber;
+
+            Physics.IgnoreCollision(
+                heldObj.GetComponent<Collider>(),
+                player.GetComponent<Collider>(),
+                true
+            );
+        }
+    }
+
+    void DropObject()
+    {
+        Physics.IgnoreCollision(
+            heldObj.GetComponent<Collider>(),
+            player.GetComponent<Collider>(),
+            false
+        );
+
+        heldObj.layer = 0;
+        heldObjRb.isKinematic = false;
+        heldObj.transform.parent = null;
+        Collider[] hits = Physics.OverlapSphere(heldObj.transform.position, 0.01f);
+
+        foreach (Collider hit in hits)
+        {
+            if (hit.CompareTag("Wall") || hit.CompareTag("Ground"))
+            {
+                heldObj.transform.position = player.transform.position;
+                break;
+            }
+        }
+        heldObj = null;
+    }
+
+    void MoveObject()
+    {
+        heldObj.transform.position = Vector3.Lerp(heldObj.transform.position, holdPos.transform.position, 20*Time.deltaTime);
         
     }
+    void StopClipping()
+    {
+        var clipRange = Vector3.Distance(
+            heldObj.transform.position,
+            transform.position
+        );
+
+        RaycastHit[] hits;
+
+        hits = Physics.RaycastAll(
+            transform.position,
+            transform.TransformDirection(Vector3.forward),
+            clipRange
+        );
+
+        if (hits.Length > 1)
+        {
+            heldObj.transform.position =
+                transform.position + new Vector3(0f, -0.5f, 0f);
+        }
+    }
+
 }
---
+++ Assets/Scripts/RigidItems.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+
+public class RigidItems : MonoBehaviour
+{
+    private Rigidbody rb;
+    private Collider col;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        col = GetComponent<Collider>();
+    }
+
+    public void ResolvePenetration()
+    {
+        Physics.SyncTransforms();
+
+        for (int i = 0; i < 10; i++)
+        {
+            Collider[] hits = Physics.OverlapBox(
+                col.bounds.center,
+                col.bounds.extents,
+                transform.rotation
+            );
+
+            bool pushed = false;
+
+            foreach (Collider hit in hits)
+            {
+                if (hit == col)
+                    continue;
+
+                if (!hit.CompareTag("Wall") && !hit.CompareTag("Ground"))
+                    continue;
+
+                Vector3 direction;
+                float distance;
+
+                if (Physics.ComputePenetration(
+                    col,
+                    transform.position,
+                    transform.rotation,
+                    hit,
+                    hit.transform.position,
+                    hit.transform.rotation,
+                    out direction,
+                    out distance))
+                {
+                    transform.position += direction * (distance + 0.01f);
+                    pushed = true;
+                }
+            }
+
+            if (!pushed)
+                break;
+        }
+
+        Physics.SyncTransforms();
+    }
+}
