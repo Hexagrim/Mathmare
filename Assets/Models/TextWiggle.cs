6bc1ab43786ba7a36b7c76bced2b0a358befcef7
HexaGrim <hexagrim3456@gmail.com>
2026-09-20 20:28:01 +0545

    ok fr .5 update ig, uh finalised the UI with animations for now, made 2 new scripts but no code so rest is for next hours

--- Assets/Models/TextWiggle.cs
+++ Assets/Models/TextWiggle.cs
@@ -1,16 +1,49 @@
+using TMPro;
 using UnityEngine;
 
 public class TextWiggle : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public TMP_Text text;
+    public float rotation = 5f;
 
-    // Update is called once per frame
     void Update()
     {
-        
+        text.ForceMeshUpdate();
+
+        TMP_TextInfo info = text.textInfo;
+
+        for (int i = 0; i < info.characterCount; i++)
+        {
+            TMP_CharacterInfo character = info.characterInfo[i];
+
+            if (!character.isVisible)
+                continue;
+
+            int materialIndex = character.materialReferenceIndex;
+            int vertexIndex = character.vertexIndex;
+
+            Vector3[] vertices = info.meshInfo[materialIndex].vertices;
+
+            Vector3 center = (vertices[vertexIndex] +
+                              vertices[vertexIndex + 2]) * 0.5f;
+
+            float angle = Random.Range(-rotation, rotation);
+
+            Matrix4x4 matrix = Matrix4x4.TRS(
+                center,
+                Quaternion.Euler(0, 0, angle),
+                Vector3.one
+            );
+
+            for (int j = 0; j < 4; j++)
+                vertices[vertexIndex + j] =
+                    matrix.MultiplyPoint3x4(vertices[vertexIndex + j]);
+        }
+
+        for (int i = 0; i < info.meshInfo.Length; i++)
+        {
+            info.meshInfo[i].mesh.vertices = info.meshInfo[i].vertices;
+            text.UpdateGeometry(info.meshInfo[i].mesh, i);
+        }
     }
 }
